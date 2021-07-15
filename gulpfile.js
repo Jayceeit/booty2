410d2a4922b344029fae1ac01151d8125eb7e3ed
Jayceeit <48262339+Jayceeit@users.noreply.github.com>
2021-07-15 13:19:00 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -350,3 +350,18 @@ gulp.task('package', gulp.series('cleanup-dist'))
 gulp.task('publish', gulp.series('add-appcache-manifest', 'generate-service-worker'))
 
 gulp.task('default', gulp.series('build'))
+
+//ADDING GITHUB PAGES CODE HERE!!
+//HERE~
+
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
